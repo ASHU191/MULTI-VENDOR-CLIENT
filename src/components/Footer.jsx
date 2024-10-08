a4da6effdcf998a666926f77a186d39803da8827
MUHAMMAD ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2024-10-08 20:09:27 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
             <div className='w-[85%] flex flex-wrap mx-auto border-b py-16 md-lg:pb-10 sm:pb-6'>
                 <div className='w-3/12 lg:w-4/12 sm:w-full'>
                     <div className='flex flex-col gap-3'>
-                        <img className='w-[190px] h-[70x]' src="http://localhost:3000/images/logo.png" alt="logo" />
+                        <img className='w-[190px] h-[70x]' src="/images/logo.png" alt="logo" />
                         <ul className='flex flex-col gap-2 text-gray-300'>
                             <li>Address : Karachi,Pakistan</li>
                             <li>Phone : +923010209887</li>
