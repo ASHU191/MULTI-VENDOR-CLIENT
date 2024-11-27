9c1f005b362befd36e5c2d62964deec82455ba33
MUHAMMAD ARSALAN AFTAB <106240658+ASHU191@users.noreply.github.com>
2024-11-27 18:32:18 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     screens: {
       'xl': { 'max': '1200px' },
       'lg': { 'max': '1080px' },
-      'md-lg': { 'max': '991px' },
+      'md-lg': { 'max': '991px' },  
       'md': { 'max': '769px' },
       'sm': { 'max': '576px' },
       'xs': { 'max': '480px' },
